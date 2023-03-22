fa2f57577ab57eef3f169eacf511c58fba6ad44b
Derek Chen <demiao.chen@student.unsw.edu.au>
2023-03-22 17:35:37 +1100

    refactor js css files

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,3 +1,5 @@
+import { BACKEND_PORT } from "./config.js";
+
 /**
  * Given a js file object representing a jpg or png image, such as one taken
  * from a html file input element, return a promise which resolves to the file
@@ -6,7 +8,7 @@
  *   https://developer.mozilla.org/en-US/docs/Web/API/File
  *   https://developer.mozilla.org/en-US/docs/Web/API/FileReader
  *   https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
- * 
+ *
  * Example Usage:
  *   const file = document.querySelector('input[type="file"]').files[0];
  *   console.log(fileToDataUrl(file));
@@ -20,7 +22,7 @@ export function fileToDataUrl(file) {
     if (!valid) {
         throw Error('provided file is not a png, jpg or jpeg image.');
     }
-    
+
     const reader = new FileReader();
     const dataUrlPromise = new Promise((resolve,reject) => {
         reader.onerror = reject;
@@ -29,3 +31,46 @@ export function fileToDataUrl(file) {
     reader.readAsDataURL(file);
     return dataUrlPromise;
 }
+
+export const apiCall = (path, method, body, headers = {}) => {
+    console.log("API call:", path, method, body);
+
+    const options = {
+        method: method,
+        headers: {
+            "Content-type": "application/json",
+            ...headers, // Merge custom headers
+        },
+    };
+
+    if (method === "GET" && body ) {
+        // Convert body object to query string for GET requests
+        const queryString = new URLSearchParams(body).toString();
+        if (queryString)
+            path += "?" + queryString;
+    } else if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+    if (localStorage.getItem("token")) {
+        options.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    }
+
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:${BACKEND_PORT}/` + path, options)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.error) {
+                    showErrorPopup(data.error);
+                } else {
+                    resolve(data);
+                }
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+                reject(error);
+            });
+    });
+};
