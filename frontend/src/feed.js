7f81442b3965e06b998dfa1797699f4971ec9f68
Derek Chen <demiao.chen@student.unsw.edu.au>
2023-03-23 17:32:40 +1100

    Finish Comment, Like for Job cards. Milestone 3

--- frontend/src/feed.js
+++ frontend/src/feed.js
@@ -1,5 +1,5 @@
 
-import { apiCall } from "./helpers.js";
+import { apiCall, show } from "./helpers.js";
 
 export const populateFeed = async () => {
     const data = await apiCall("job/feed?start=0", "GET", {});
@@ -17,10 +17,13 @@ export const populateFeed = async () => {
         colImg.className = "col-md-4";
         row.appendChild(colImg);
 
+        const imgWrapper = document.createElement("div");
+        imgWrapper.className = "card-img img-wrapper";
+        colImg.appendChild(imgWrapper);
         const img = document.createElement("img");
         img.src = feedItem.image;
-        img.className = "card-img job-image";
-        colImg.appendChild(img);
+        img.className = "job-image";
+        imgWrapper.appendChild(img);
 
         const colBody = document.createElement("div");
         colBody.className = "col-md-8";
@@ -40,74 +43,76 @@ export const populateFeed = async () => {
         description.textContent = feedItem.description;
         cardBody.appendChild(description);
 
-        // creator and post time
-        const creatorAndTime = document.createElement("p");
-        creatorAndTime.className = "card-text creator-time-wrapper";
-
-        const creatorText = document.createElement("small");
-        creatorText.className = "text-muted creator-text";
-        creatorText.textContent = "Created by: " + await getCreatorUsername(feedItem.creatorId);
-        creatorAndTime.appendChild(creatorText);
-
-        const createTimeText = document.createElement("small");
-        createTimeText.className = "text-muted create-time-text";
-        createTimeText.textContent = "Posted at: " + formatCreationTime(feedItem.createdAt);
-        creatorAndTime.appendChild(createTimeText);
-
-        cardBody.appendChild(creatorAndTime);
+        // creator, post time and starting date
+        const extraInfo = document.createElement("div");
+        extraInfo.className = "creator-time-wrapper";
+        cardBody.appendChild(extraInfo);
+        const creatorText = createInfoTextElement("Created by: " + await getCreatorUsername(feedItem.creatorId), "card-text text-muted");
+        extraInfo.appendChild(creatorText);
+        const createTimeText = createInfoTextElement("Posted at: " + formatTime(feedItem.createdAt), "card-text text-muted");
+        extraInfo.appendChild(createTimeText);
+        const startingDateText = createInfoTextElement("Starting date: " + formatTime(feedItem.start), "card-text text-muted");
+        extraInfo.appendChild(startingDateText);
 
         // like and comment buttons
         const actionsRow = document.createElement("div");
-        actionsRow.className = "d-flex justify-content-start align-items-center mt-2";
+        actionsRow.className = "d-flex justify-content-start align-items-center mt-2 actions-row";
         cardBody.appendChild(actionsRow);
 
+        // like button, badge and event listener
         const likeButton = document.createElement("button");
         likeButton.className = "btn btn-outline-primary btn-sm me-2 like-button";
         actionsRow.appendChild(likeButton);
-
-        // font awesome icon
-        const likeIcon = document.createElement("i");
-        likeIcon.className = "fas fa-thumbs-up"; //
+        const likeIcon = document.createElement("i"); // font awesome icon
+        likeIcon.className = "fas fa-thumbs-up";
         likeButton.appendChild(likeIcon);
-
         const likeText = document.createTextNode(" Likes ");
         likeButton.appendChild(likeText);
-
         const likeBadge = document.createElement("span");
-        likeBadge.className = "badge bg-primary";
+        likeBadge.className = "badge bg-danger like-badge";
         likeBadge.textContent = feedItem.likes.length;
         likeButton.appendChild(likeBadge);
-
-        const current_user_id = localStorage.getItem("userId");
-        const userHasLiked = feedItem.likes.includes(current_user_id);
+        likeBadge.addEventListener("click", (event) => {
+            event.stopPropagation(); // Prevent button click event from being triggered
+            // pop up people who liked this post box
+            const likedBy = feedItem.likes.map(user => user.userName)
+            popupLikeList(likedBy);
+        });
+        const currentUserId = localStorage.getItem("userId");
+        const userHasLiked = feedItem.likes.find(user => user.userId == currentUserId);
         toggleLikeButton(likeButton, userHasLiked);
         likeButton.addEventListener('click', () => {
-            const liked = feedItem.likes.includes(current_user_id);
+            const liked = feedItem.likes.find(user => user.userId == currentUserId);
+            console.log(liked);
             if (liked) {
                 apiCall(`job/like`, "PUT", {"id": feedItem.id, "turnon": false});
             } else {
                 apiCall(`job/like`, "PUT", {"id": feedItem.id, "turnon": true});
             }
             likeBadge.textContent = feedItem.likes.length;
-            const userLiked = feedItem.likes.includes(current_user_id);
+            const userLiked = feedItem.likes.find(user => user.userId == currentUserId);
             toggleLikeButton(likeButton, userLiked);
         });
 
+        // comment button, badge and event listener
         const commentButton = document.createElement("button");
         commentButton.className = "btn btn-outline-secondary btn-sm comment-button";
         actionsRow.appendChild(commentButton);
-
         const commentIcon = document.createElement("i");
         commentIcon.className = "fas fa-comment";
         commentButton.appendChild(commentIcon);
-
         const commentText = document.createTextNode(" Comments ");
         commentButton.appendChild(commentText);
-
         const commentBadge = document.createElement("span");
         commentBadge.className = "badge bg-secondary";
         commentBadge.textContent = feedItem.comments.length;
         commentButton.appendChild(commentBadge);
+        commentButton.addEventListener("click", (event) => {
+            // clear comment input
+            document.getElementById("comment-input").value = "";
+            // pop up comments box
+            popupCommentList(feedItem.comments, feedItem.id);
+        });
 
         document.getElementById("feed-items").appendChild(feedDom);
     }
@@ -119,7 +124,7 @@ const getCreatorUsername = async (id) => {
     return data.name;
 };
 
-const formatCreationTime = (createAt) => {
+const formatTime = (createAt) => {
     const now = new Date();
     const createdDate = new Date(createAt);
     const diffInMs = now - createdDate;
@@ -138,6 +143,12 @@ const formatCreationTime = (createAt) => {
     }
 };
 
+const createInfoTextElement = (text, className) => {
+    const paragraph = document.createElement("p");
+    paragraph.className = className;
+    paragraph.textContent = text;
+    return paragraph;
+};
 
 const toggleLikeButton = (button, liked) => {
     if (liked) {
@@ -148,3 +159,60 @@ const toggleLikeButton = (button, liked) => {
       button.classList.add('btn-outline-primary');
     }
 };
+
+const showPopup = (id) => {
+    document.getElementById(id).style.display = "block";
+};
+
+
+const popupLikeList = (likedBy) => {
+    const likeList = document.getElementById("like-list");
+    likeList.innerHTML = '';
+
+    likedBy.forEach(name => {
+        const listItem = document.createElement("li");
+        listItem.className = "list-group-item";
+        listItem.textContent = name;
+        likeList.appendChild(listItem);
+    });
+
+    showPopup("like-list-popup");
+};
+
+document.getElementById("like-close-btn").addEventListener("click", () => {
+    document.getElementById("like-list-popup").style.display = "none";
+});
+
+const popupCommentList = async (comments, postId) => {
+    const commentList = document.getElementById("comment-list");
+
+    comments.forEach(comment => {
+        const listItem = document.createElement("li");
+        listItem.className = "list-group-item";
+        listItem.textContent = comment.userName + ': ' + comment.comment;
+        commentList.appendChild(listItem);
+    });
+
+    document.getElementById("comment-button").addEventListener("click", () => {
+        const comment = document.getElementById("comment-input").value;
+        if (comment) {
+            apiCall(`job/comment`, "POST", {"id": postId, "comment": comment});
+            document.getElementById("comment-input").value = "";
+        }
+    });
+
+    // User can press enter to submit comment
+    const commentInput = document.getElementById("comment-input");
+    commentInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent the default action (newline)
+            document.getElementById("comment-button").click();
+        }
+    });
+
+    showPopup("comment-list-popup");
+};
+
+document.getElementById("comment-close-btn").addEventListener("click", () => {
+    document.getElementById("comment-list-popup").style.display = "none";
+});
